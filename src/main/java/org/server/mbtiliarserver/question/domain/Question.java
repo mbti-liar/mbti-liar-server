0f274086c595f82471e20cc7219c0f388886e220
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2022-08-13 21:22:11 +0900

    Feat : Question 기능 구현 및 테스트

---
+++ src/main/java/org/server/mbtiliarserver/question/domain/Question.java
@@ -0,0 +1,20 @@
+package org.server.mbtiliarserver.question.domain;
+
+public class Question {
+
+    Long id;
+    String question;
+
+    public Question(Long id, String question) {
+        this.id = id;
+        this.question = question;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+}
