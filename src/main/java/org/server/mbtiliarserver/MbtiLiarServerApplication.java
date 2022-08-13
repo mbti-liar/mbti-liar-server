f7064bb82a573dd6e18734289036f6fcec3f43f4
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-13 16:47:53 +0900

    Init : 초기 설정 및 패키지 구성

---
+++ src/main/java/org/server/mbtiliarserver/MbtiLiarServerApplication.java
@@ -0,0 +1,13 @@
+package org.server.mbtiliarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiLiarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiLiarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/server/mbtiliarserver/MbtiLiarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.server.mbtiliarserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiLiarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
