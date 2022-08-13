3379935e1cf4868c250340a7ba08cfd18f71b91f
tis <92219795+this-is-spear@users.noreply.github.com>
2022-08-13 18:18:25 +0900

    Refact : GameRoomController 리퀘스트 매핑 수정

--- src/main/java/org/server/mbtiliarserver/gameroom/ui/GameRoomController.java
+++ src/main/java/org/server/mbtiliarserver/gameroom/ui/GameRoomController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("games")
+@RequestMapping("gamerooms")
 public class GameRoomController {
 
     private final GameRoomService gameService;
